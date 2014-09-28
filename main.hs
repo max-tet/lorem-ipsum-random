8d596609adf6b477d19f9be58ca7b4c3649fbc89
Max von Tettenborn <contact@max-tettenborn.de>
2014-09-28 18:49:19 +0200

    decomposing and cleaning of input string

--- main.hs
+++ main.hs
@@ -1,5 +1,12 @@
 import qualified WList as WL
+import Data.List.Split (splitOn)
+import Data.List (intersect)
+
+allowedChars = ['A'..'Z']++['a'..'z']
 
 main = do
 	input <- getContents
-	putStr $ show $ length input
+	putStrLn $ show $ (cleanup . decompose) input
+
+decompose = (map (splitOn " ")) . (splitOn ".")
+cleanup = filter (\x -> length x > 0) . (map $ filter (\x -> length x > 0) . map (filter (`elem` allowedChars)))
